bf08f2e11580ad70a370b9e120c17f026636df7f
MaartenGDev <maarten.grootoonk@gmail.com>
2017-05-16 21:57:12 +0200

    Added tests for html parser

--- tests/htmlParser.test.js
+++ tests/htmlParser.test.js
@@ -1,15 +1,107 @@
 import htmlParser from '../src/htmlParser';
 
 const parser = new DOMParser();
-const doc = parser.parseFromString(`<main class="app"><h1>test</h1></main>`, "text/html");
-
-const rootElement = doc.querySelector('.app');
 
 test('should return h1 json object', () => {
+    const rootElement = parser
+        .parseFromString(`<main class="app"><h1>test</h1></main>`, "text/html")
+        .querySelector('.app');
+
 
     expect(htmlParser(rootElement.children)).toEqual([{
-        attributes: {},
         type: 'h1',
-        value: 'test'
+        value: 'test',
+        attributes: {}
+    }]);
+});
+
+test('section with html attributes is serialized correctly', () => {
+    const rootElement = parser
+        .parseFromString(`<main class="app"><section data-translation-id="35" data-section-type="header">test</section></main>`, "text/html")
+        .querySelector('.app');
+
+
+    expect(htmlParser(rootElement.children)).toEqual([{
+        type: 'section',
+        value: 'test',
+        attributes: {
+            "data-translation-id": "35",
+            "data-section-type": "header"
+        }
+    }]);
+
+});
+
+test('element with empty content returns empty string', () => {
+    const rootElement = parser
+        .parseFromString(`<main class="app"><section></section></main>`, "text/html")
+        .querySelector('.app');
+
+    expect(htmlParser(rootElement.children)).toEqual([{
+        type: 'section',
+        value: '',
+        attributes: {}
+    }]);
+});
+
+test('spacing in node value gets removed', () => {
+    const rootElement = parser
+        .parseFromString(`<main class="app"><section>spacing </section></main>`, "text/html")
+        .querySelector('.app');
+
+    expect(htmlParser(rootElement.children)).toEqual([{
+        type: 'section',
+        value: 'spacing',
+        attributes: {}
+    }]);
+});
+
+test('spacing in node value gets removed', () => {
+    const rootElement = parser
+        .parseFromString(`<main class="app"><nav>hello <h1 class="brand">My Brand</h1></nav></main>`, "text/html")
+        .querySelector('.app');
+
+    expect(htmlParser(rootElement.children)).toEqual([{
+        type: 'nav',
+        value: 'hello',
+        attributes: {},
+        children: [
+            {
+                type: 'h1',
+                value: 'My Brand',
+                attributes: {
+                    class: "brand"
+                }
+            }
+        ]
+    }]);
+});
+
+test('header with links is serialized correctly', () => {
+    const rootElement = parser
+        .parseFromString(`<main class="app"><header><ul><li>Test</li><li>Test2</li></ul></header></main>`, "text/html")
+        .querySelector('.app');
+
+    expect(htmlParser(rootElement.children)).toEqual([{
+        type: 'header',
+        value: '',
+        attributes: {},
+        children: [{
+            type: 'ul',
+            value: '',
+            attributes: {},
+            children: [
+                {
+                    type: 'li',
+                    value: 'Test',
+                    attributes: {}
+                },
+                {
+                    type: 'li',
+                    value: 'Test2',
+                    attributes: {}
+                }
+            ],
+        }]
     }]);
 });
